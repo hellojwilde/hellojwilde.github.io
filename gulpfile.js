408ca5d68c81a2ed4ec26689438d702782354e67
Jonathan Wilde <hello@jwilde.me>
2015-06-29 00:11:42 -0700

    Make bundling something super easy.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+
+var autoprefixer = require('gulp-autoprefixer');
+var merge = require('merge-stream');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var rework = require('gulp-rework');
+var reworkImport = require('rework-import');
+var reworkInline = require('rework-plugin-inline');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('build', function() {
+  return gulp.src(['assets/styles/jwilde.css'])    
+    .pipe(rework(reworkInline('assets/images'), reworkImport(), {sourcemap: true}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(autoprefixer())
+      .pipe(minify())
+      .pipe(rename('bundle.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', function() {
+  gulp.watch(['assets/images/*', 'assets/styles/*'], ['build']);
+});
